7141df8c7862da767d5736d42d1a66a0b2880730
giujae <alacran6924@gmail.com>
2025-06-06 19:54:18 +0900

    feat/#5 : 홈컴포넌트에 기술 스택 컴포넌트>를 추가한다

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import IntroSection from '@/components/sections/IntroSection';
+import TechStackSection from '@/components/sections/TechStackSection';
 
 export default function Home() {
     return (
         <main>
             <IntroSection />
+            <TechStackSection />
         </main>
     );
 }
