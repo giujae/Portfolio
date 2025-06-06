26a6f6b46898803cde538b58b19de5192a74fad3
giujae <alacran6924@gmail.com>
2025-06-06 20:09:44 +0900

    feat/#6 : 홈 컴포넌트에 프로젝트 섹션 컴포넌트를 추가한다

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import IntroSection from '@/components/sections/IntroSection';
 import TechStackSection from '@/components/sections/TechStackSection';
+import ProjectSection from '@/components/sections/ProjectSection';
 
 export default function Home() {
     return (
         <main>
             <IntroSection />
             <TechStackSection />
+            <ProjectSection />
         </main>
     );
 }
