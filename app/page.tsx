b492b19a574d89434e72cec15048fc07c6e5426f
giujae <alacran6924@gmail.com>
2025-06-07 23:42:05 +0900

    feat/#10 : 인터뷰 섹션을 홈 컴포넌트에 추가한다

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import IntroSection from '@/components/sections/IntroSection';
+import InterviewSection from '@/components/sections/InterviewSection';
 import TechStackSection from '@/components/sections/TechStackSection';
 import ProjectSection from '@/components/sections/ProjectSection';
 import EducationSection from '@/components/sections/EducationSection';
@@ -8,6 +9,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between">
             <IntroSection />
+            <InterviewSection />
             <TechStackSection />
             <ProjectSection />
             <EducationSection />
