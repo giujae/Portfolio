a793f8955cfab131655c8b7ef4f3bcfbd0a5e273
giujae <alacran6924@gmail.com>
2025-06-11 09:22:03 +0900

    refactor/#13 : 컴포넌트의 이름을 명확하게 변경한다

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import IntroSection from '@/components/sections/IntroSection';
-import InterviewSection from '@/components/sections/InterviewSection';
-import TechStackSection from '@/components/sections/TechStackSection';
-import ProjectSection from '@/components/sections/ProjectSection';
 import EducationSection from '@/components/sections/EducationSection';
 import FooterSection from '@/components/sections/FooterSection';
+import IntroSection from '@/components/sections/IntroSection';
+import InterviewSection from '@/components/sections/InterviewSection';
+import ProjectSection from '@/components/sections/ProjectSection';
+import TechStackSection from '@/components/sections/TechStackSection';
 
-export default function Home() {
+export default function HomePage() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between">
             <IntroSection />
